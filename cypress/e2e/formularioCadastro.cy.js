af9f832c24701d5050c35c457628b3d07c20238e
daniperino <daniperino@gmail.com>
2023-03-31 17:19:07 -0300

    add tests

--- cypress/e2e/formularioCadastro.cy.js
+++ cypress/e2e/formularioCadastro.cy.js
@@ -5,12 +5,12 @@ describe('formulário Cadastro',()=>{
     it('O usuário deve conseguir se cadastrar com sucesso', () => {
         cy.getByData('botao-cadastro').click()
         cy.getByData('nome-input').type('Daniella Perino')
-        cy.getByData('email-input').type('dan@gmail.com')
+        cy.getByData('email-input').type('dani@gmail.com')
         cy.getByData('senha-input').type('Teste@123')
         cy.getByData('checkbox-input').click()
         cy.getByData('botao-enviar').click()
         cy.getByData('mensagem-sucesso').should('exist').end('have.text', 'Usuário cadastrado com successo')
-
+        
 
     });
 })
---
+++ cypress/e2e/formularioErroCadastro.cy.js
@@ -0,0 +1,8 @@
+/*describe('Erro no cadastro',()=>{
+    beforeEach(()=>{
+        cy.visit('http://localhost:3000/')
+    })
+    it('Não deve permitir dados invalidos', () => {
+        
+    });
+})
